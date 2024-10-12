f91679ee90544985a4d4cdda8acfd60725dcb667
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2024-10-12 13:54:47 +0600

    TopBar added

---
+++ src/components/TopBar/TopBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TopBar = () => {
+    return (
+        <div className='flex justify-around p-4 bg-gray-100 shadow-md'>
+            <button className="btn btn-outline btn-primary">Preview</button>
+            <button className="btn btn-outline btn-success">Source Code</button>
+        </div>
+    );
+};
+
+export default TopBar;
--- src/pages/HomePage/Home/Home.jsx
+++ src/pages/HomePage/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import SideBar from '../../../components/SideBar/SideBar';
+import TopBar from '../../../components/TopBar/TopBar';
 
 const Home = () => {
     return (
         <div>
+            <TopBar></TopBar>
             <SideBar></SideBar>
         </div>
     );
