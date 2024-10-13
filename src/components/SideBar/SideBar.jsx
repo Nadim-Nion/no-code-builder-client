418d5086ae14b9ec47702e3299ffa2d4dcdd1003
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2024-10-13 11:14:40 +0600

    drag and drop implemented in sidebar

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,27 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDrag } from 'react-dnd';
 import { CiImageOn, CiText } from 'react-icons/ci';
 
+const draggableItemsTypes = {
+    TEXT: 'text',
+    IMAGE: 'image'
+};
+
+const DraggableItems = ({ type, children }) => {
+
+    const [{ isDragging }, drag] = useDrag({
+        type,
+        item: { type },
+        collect: (monitor) => (
+            {
+                isDragging: !!monitor.isDragging()
+            }
+        )
+    });
+
+    return <div
+        ref={drag}
+        className={`cursor-pointer mb-2 p-2 bg-white border flex flex-col justify-center items-center ${isDragging ? 'opacity-50' : ''}`}
+    >
+        {children}
+    </div>;
+}
+
 const SideBar = () => {
+    const [selected, setSelected] = useState(null);
+
+    const handleSelection = type => {
+        setSelected(type);
+    }
 
     return (
         <div className='p-4 w-24 bg-gray-200'>
             {/* Text */}
-            <div
-                className='cursor-pointer mb-2 p-2 bg-white border flex flex-col justify-center items-center'
-                draggable
-            >
+            <DraggableItems type={draggableItemsTypes.TEXT} onClick={() => handleSelection('text')}>
                 <CiText className='text-lg' />
                 <span className='text-lg'>Text</span>
-            </div>
+            </DraggableItems>
 
             {/* Image */}
-            <div
-                className='cursor-pointer mb-2 p-2 bg-white border flex flex-col justify-center items-center'
-                draggable
-            >
+            <DraggableItems type={draggableItemsTypes.IMAGE} onClick={() => handleSelection('image')}>
                 <CiImageOn className='text-lg' />
                 <span className='text-lg'>Image</span>
-            </div>
+            </DraggableItems>
+
+            {/* Submenu for Text Element */}
+            {
+                selected === 'text' && <>
+                    <div className='mt-4 p-4 bg-white border rounded'>
+                        <h3 className='mb-2 font-bold'>Text Options</h3>
+                    </div>
+                </>
+            }
+
+            {/* Submenu for Image Element */}
+            {
+                selected === 'image' && <>
+                    <div className='mt-4 p-4 bg-white border rounded'>
+                        <h3 className='mb-2 font-bold'>Image Options</h3>
+                    </div>
+                </>
+            }
         </div>
     );
 };
--- src/pages/HomePage/Home/Home.jsx
+++ src/pages/HomePage/Home/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import SideBar from '../../../components/SideBar/SideBar';
 import TopBar from '../../../components/TopBar/TopBar';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 
 const Home = () => {
     return (
         <div>
             <TopBar></TopBar>
-            <SideBar></SideBar>
+            <DndProvider backend={HTML5Backend}>
+                <SideBar></SideBar>
+            </DndProvider>
         </div>
     );
 };
